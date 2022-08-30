68f6cfaee7c810afa1647c0bebbe065c11f9620e
Yahya Ghoor <ghooryahya@gmail.com>
2022-08-30 12:19:52 +0200

    Setup App.js with routing

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import YearsList from './components/YearsList';
+import YearInfo from './components/YearInfo';
+
+const App = () => (
+  <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<YearsList />} />
+        <Route path=":slug" element={<YearInfo />} />
+      </Routes>
+    </Router>
+  </>
+);
+
+export default App;
