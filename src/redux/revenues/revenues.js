da9d47e9a7263efbcdd126b854276e8d86ed7004
Yahya Ghoor <ghooryahya@gmail.com>
2022-08-30 23:03:04 +0200

    Create reducer to add revenues to state

--- src/redux/revenues/revenues.js
+++ src/redux/revenues/revenues.js
@@ -1,4 +1,15 @@
 const RETRIEVE_REVENUES = 'metrics-webapp/revenues/RETRIEVE_REVENUES';
+const initialState = [];
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case RETRIEVE_REVENUES:
+      return [...state, ...action.payload];
+  }
+};
+
+export default reducer;
+
 export const retrieveRevenues = () => async (dispatch) => {
     const revenues = await fetch(
       'https://financialmodelingprep.com/api/v4/revenue-geographic-segmentation?symbol=AAPL&structure=flat&apikey=3b6685977c96f27f3e1df3d5e42bd7e5',
